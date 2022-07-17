2b85dc34f53a43349b60a832b627aee23b3a8aec
Rizwan Mushtaq <rizwan.mushtaq@ottonova.de>
2022-07-17 16:37:47 +0200

    adding remote db details

--- mysqlConfig.ts
+++ mysqlConfig.ts
@@ -3,13 +3,26 @@ import { User } from './src/users/entities/user.entity';
 import { Todo } from './src/todos/entities/todo.entity';
 
 //database settings
+// const mysqlConfig: MysqlConnectionOptions = {
+//   type: 'mysql',
+//   host: 'localhost',
+//   port: 8889,
+//   username: 'root',
+//   password: 'root',
+//   database: 'doitnow',
+//   entities: [User, Todo],
+//   // entities: ['dist/src/**/*.entity.js'],
+//   // entities: [__dirname + '/../**/*.entity{.ts,.js'],
+//   synchronize: true,
+// };
+
 const mysqlConfig: MysqlConnectionOptions = {
   type: 'mysql',
-  host: 'localhost',
-  port: 8889,
-  username: 'root',
-  password: 'root',
-  database: 'doitnow',
+  host: 'oliadkuxrl9xdugh.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
+  port: 3306,
+  username: 'ftbsofct0we5g4uh',
+  password: 'pdnmbokgbc4pylp2',
+  database: 'iylb880pcgd1781u',
   entities: [User, Todo],
   // entities: ['dist/src/**/*.entity.js'],
   // entities: [__dirname + '/../**/*.entity{.ts,.js'],
